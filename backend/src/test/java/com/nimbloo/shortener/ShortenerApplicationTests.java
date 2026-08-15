f385ba440e38eb743e3a23f2fa604b6ceef599b3
Giovanny Lira de Araujo Cunha <giovannyaraujo29@gmail.com>
2026-08-15 14:08:53 -0300

    fix: make context load test independent of local infrastructure

--- backend/src/test/java/com/nimbloo/shortener/ShortenerApplicationTests.java
+++ backend/src/test/java/com/nimbloo/shortener/ShortenerApplicationTests.java
@@ -2,9 +2,15 @@ package com.nimbloo.shortener;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.nimbloo.shortener.consumer.SqsClickConsumer;
 
 @SpringBootTest
 class ShortenerApplicationTests {
+
+	@MockBean
+	private SqsClickConsumer sqsClickConsumer;
 
 	@Test
 	void contextLoads() {
